3c900d2df9ff10c2d80c49d30300f48b40fe536d
Siamak Samie <siamak.samie@gmail.com>
2019-01-04 06:03:33 -0500

    Updates

---
+++ precache-manifest.eb31c00cd6f03a2f80d14939f916dc29.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9902959729233d364b6ddf1ebde7754c",
+    "url": "/Kdy/static/media/mtl4.99029597.jpg"
+  },
+  {
+    "revision": "5f31728e824b16690fb90b44d6192e70",
+    "url": "/Kdy/static/media/mtl3.5f31728e.jpg"
+  },
+  {
+    "revision": "a255e3c62afb429f75806c28bc34c477",
+    "url": "/Kdy/static/media/mtl.a255e3c6.png"
+  },
+  {
+    "revision": "055364f556efd2e4ac0b392df09e8125",
+    "url": "/Kdy/static/media/logoWhite.055364f5.png"
+  },
+  {
+    "revision": "d259d082ec243aef4e44",
+    "url": "/Kdy/static/js/runtime~main.d259d082.js"
+  },
+  {
+    "revision": "d611b08a1d106058cc8e",
+    "url": "/Kdy/static/js/main.d611b08a.chunk.js"
+  },
+  {
+    "revision": "fda2418580712f5c9124",
+    "url": "/Kdy/static/js/1.fda24185.chunk.js"
+  },
+  {
+    "revision": "d611b08a1d106058cc8e",
+    "url": "/Kdy/static/css/main.f3995bcd.chunk.css"
+  },
+  {
+    "revision": "b7b7f1ab48f63dec45912fe2460eca97",
+    "url": "/Kdy/index.html"
+  }
+];
